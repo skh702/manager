799483fa695c4e7f06ae57976696c24090ff7d62
skh702 <seopd_1004@yahoo.co.jp>
2022-01-13 17:37:28 +0900

    Create Customer Database Table NOde.js database connection settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    database: conf.database,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port
+})
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id':1,
-            'image':'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday':'9622441',
-            'gender':'남자',
-            'job':'대학생'
-          },
-          {
-            'id':2,
-            'image':'https://placeimg.com/64/64/2',
-            'name': '고길동',
-            'birthday':'9222441',
-            'gender':'남자',
-            'job':'프로그래머'
-          },
-          {
-            'id':3,
-            'image':'https://placeimg.com/64/64/3',
-            'name': '저글링',
-            'birthday':'8322441',
-            'gender':'여자',
-            'job':'섹시'
-          }
-    ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) =>{
+            res.send(rows);
+        }
+    )
+
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
