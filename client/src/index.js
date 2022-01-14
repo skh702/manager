3ffad44fdd9042ff5d087c19df08fe24c11fdf74
skh702 <seopd_1004@yahoo.co.jp>
2022-01-14 18:31:37 +0900

    Update UI/UX

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
 
+const theme = createTheme({
+  typography:{
+    fontFamily:'"NOTO Sans KR","serif"'
+  }
+})
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}><App /></ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
